d9696281e9676478c075a6a0d84e840714a776ce
Marek Janda <jandamarek92@gmail.com>
2021-08-16 16:34:43 +0100

    Basic flask config

--- application.py
+++ application.py
@@ -1,1 +1,9 @@
 # Flask app to work as a web socket for for data transmission
+import os
+
+from flask import Flask, session, jsonify, redirect, request
+from flask_session import Session
+from flask_api import status
+import requests
+
+app = Flask(__name__)
--- pull.py
+++ pull.py
@@ -1,1 +1,6 @@
 # Script to get the temperature data from the server and writing into a text file
+from temp import temperatures
+
+for key in temperatures:
+    print(key)
+
