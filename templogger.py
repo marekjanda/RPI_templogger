706aac25c3160b9aa69d097366d055431ee3ccf8
Marek Janda <jandamarek92@gmail.com>
2021-08-17 15:27:01 +0100

    Temperature transmitting and writing values to a txt file

--- templogger.py
+++ templogger.py
@@ -2,49 +2,30 @@
 # All temperatures are in DegC
 
 import asyncio
-from json.encoder import JSONEncoder
 import websockets
 import json
 import time
 
-# This definition will come from real temp. monitoring
-T01 = 84 
-T02 = 85 
-T03 = 86
-T04 = 82
-T05 = 85
-T06 = 86
-T07 = 82
-T08 = 83
-T09 = 84
-T10 = 80
-T11 = 86
-T12 = 87
+def log_to_txt(temps):
+    with open("motor_temps.txt", 'w', encoding='utf-8') as f:
+        for t in temps:
+            if t == "T12":
+                f.write(f"{temps[t]}")
+            else:
+                f.write(f"{temps[t]}\n")
+    return
 
-# Create a dictionary that will be pushed to server
-temperatures = {
-    "T01": T01,
-    "T02": T01,
-    "T03": T03,
-    "T04": T04,
-    "T05": T05,
-    "T06": T06,
-    "T07": T07,
-    "T08": T08,
-    "T09": T09,
-    "T10": T10,
-    "T11": T11,
-    "T12": T12,
-}
-
-# Send temperatures to a server
+# Pull temperatures to a server
 while True:
     async def hello():
         uri = "ws://localhost:8765"
         async with websockets.connect(uri) as websocket:
             await websocket.send("pull")
             resp = await websocket.recv()
-            print(resp)
+            temperatures = json.loads(resp)
+            for key in temperatures:
+                print(f"{key} = {temperatures[key]} DegC")
+                log_to_txt(temperatures)
     
     asyncio.get_event_loop().run_until_complete(hello())
     
